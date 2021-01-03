ca2607e35a217fbfcb5a286dbcccdb2318d090dc
sullivan_lb <sullivan.lb56@gmail.com>
2021-01-04 00:28:37 +0100

    private and public bloc completed

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,12 +1,26 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
-struct{
-    public:
-
+class Matrice{
     private:
         int const m_nbColumn;
         int const m_nbRow;
+        int* m_values;
+
+    public:
+        Matrice(int const nbColumn, int const nbRow);
+        Matrice copy(Matrice const m1);
+        int getElement(int const column, int const row);
+        int setElement(int const column, int const row, int const value);
+        int getDimension();
+        int* getColumn(int const column);
+        int* getRow(int const row);
+
+        Matrice operator+(Matrice const m1);
+        Matrice operator-(Matrice const m1);
+        Matrice operator*(Matrice const m1);
+        Matrice operator/(Matrice const m1);
+
 };
 
 #endif //MATRIX_HPP

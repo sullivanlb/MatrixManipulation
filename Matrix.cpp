f7219e259985c7890863475d0a737e5983a66768
sullivan_lb <sullivan.lb56@gmail.com>
2021-01-04 14:41:09 +0100

    operators + - added and * beginned

--- Matrix.cpp
+++ Matrix.cpp
@@ -11,12 +11,79 @@ Matrix::Matrix(int const nbColumn, int const nbRow) noexcept: m_nbColumn(nbColum
     int values[nbColumn*nbRow];
 }
 
-int Matrix::getDimension()
+int Matrix::getDimension() const
 {
     return m_nbColumn * m_nbRow;
 }
 
-int* getColumn(int const column)
+int* Matrix::getColumn(int const column)
 {
+    int* columnVector {new int[m_nbRow]};
+    int index = column * m_nbRow;
+
+    for (int i {0}; i < m_nbRow; i++)
+    {
+        columnVector[i] = m_values[index + i];
+    }
+    return columnVector;
+}
+
+
+int* Matrix::getRow(int const row)
+{
+    int* rowVector {new int[m_nbColumn]};
+
+    for (int i {0}; i < m_nbColumn; i++)
+    {
+        rowVector[i] = m_values[i * m_nbRow + row];
+    }
+    return rowVector;
+}
+
+Matrix operator+(Matrix const & m1, Matrix const & m2)
+{
+    assert((m1.m_nbRow == m2.m_nbRow) && (m1.m_nbColumn == m2.m_nbColumn));
+
+    Matrix ret {m1.m_nbRow, m1.m_nbColumn};
     
+    for (int i {0}; i < sizeof(ret.m_values); i++)
+    {
+        ret.m_values[i] = m1.m_values[i] + m2.m_values[i];
+    }
+    return ret;
+}
+
+Matrix operator-(Matrix const & m1, Matrix const & m2)
+{
+    assert((m1.m_nbRow == m2.m_nbRow) && (m1.m_nbColumn == m2.m_nbColumn));
+
+    Matrix ret {m1.m_nbRow, m1.m_nbColumn};
+    
+    for (int i {0}; i < sizeof(ret.m_values); i++)
+    {
+        ret.m_values[i] = m1.m_values[i] - m2.m_values[i];
+    }
+    return ret;
+}
+
+Matrix operator*(Matrix const & m1, Matrix const & m2)
+{
+    assert(m1.m_nbColumn == m2.m_nbRow);
+
+    Matrix ret {m1.m_nbRow, m2.m_nbColumn};
+
+    int index = 0;
+    for (int i {0}; i < (m1.m_nbRow * m2.m_nbColumn); i++)
+    {
+
+        int operation {0};
+        for (int j {0}; j < m1.m_nbColumn - 1; j++)
+        {
+            for (int k {0}; k < m1.m_nbRow - 1; k++)
+            {
+                
+            }
+        }
+
+    }
 }
--- Matrix.hpp
+++ Matrix.hpp
@@ -1,6 +1,8 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
+#include <iostream>
+
 class Matrix{
     private:
         int const m_nbColumn;
@@ -11,15 +13,17 @@ class Matrix{
         Matrix() = delete;
         Matrix(Matrix const &m1) = default;
         Matrix(int const nbColumn, int const nbRow) noexcept;
-        int getDimension();
+        int getDimension() const;
         int* getColumn(int const column);
         int* getRow(int const row);
 
-        Matrix operator+(Matrix const m1);
-        Matrix operator-(Matrix const m1);
-        Matrix operator*(Matrix const m1);
-        Matrix operator/(Matrix const m1);
+        friend Matrix operator+(Matrix const & m1, Matrix const & m2);
+        friend Matrix operator-(Matrix const & m1, Matrix const & m2);
+        friend Matrix operator*(Matrix const & m1, Matrix const & m2);
+        friend Matrix operator*(Matrix const & m1, int value);
+        friend Matrix operator/(Matrix const & m1, Matrix const & m2);
         Matrix operator()(int column, int row);
+        Matrix operator<<(Matrix const & m1);
 
 };
 
